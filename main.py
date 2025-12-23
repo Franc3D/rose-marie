9760511eeb7f00f282b09cbfb7531eb9828751f3
RoyGoodman <bouladier.francis@gmail.com>
2025-12-22 23:37:04 -0500

    Added main and testing files for cegep

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 #This is the project for Rose-Marie
-import pygame
+#Starting with the cegep .txt files
+#import pygame
 
 
+def main():
+    print("Rosemarie")
+
+
+if __name__ == "__main__":
+    main()
