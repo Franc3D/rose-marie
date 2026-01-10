c75448fd74608c33bca2d366668f0cf682b197db
RoyGoodman <bouladier.francis@gmail.com>
2026-01-09 22:28:49 -0500

    Added an initial menu with file types to choose.

--- main.py
+++ main.py
@@ -2,7 +2,24 @@ import cegep
 #from cegep import read_cegep
 
 def main():
-    cegep.read_cegep()
+    print("***Programme de listage de commande***")
+    print("--------------------------------------")
+    print("Choisissez une option ci bas")
+    print("1 -Cégep de l'Outaouais")
+    print("2 -Bibliothèque de Gatineau (BdG)")
+    
+    response = input("Entrez le chiffre de l'option désiré :")
+
+    if response == "1":
+        cegep.read_cegep()
+
+    elif response == "2":
+        input("Cette option n'est pas encore disponible, appuyez sur une touche pour quitter...")
+    
+    else:
+        input("Cette option n'est pas valide, appuyez sur une touche pour quitter...")
+        
+    
     
 
 if __name__ == "__main__":
