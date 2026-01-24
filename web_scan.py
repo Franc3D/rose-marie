70ac4c1209c560528ce1773e6fff4f9a0f78a534
roygoodman <bouladier.francis@gmail.com>
2026-01-23 21:35:01 -0500

    Added some comments on how to better tackle web scraping

---
+++ web_scan.py
@@ -0,0 +1,11 @@
+import urllib
+
+def main():
+    pass
+    #Figure out how to open a webpage through a .py file 
+    #The program type is called a web scraper
+    #The Beautiful soup library is especially well suited for this task 
+
+
+if __name__ == "__main__":
+    main()
