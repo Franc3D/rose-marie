2f66002d53f985cc932f2d276a9303185a6bf155
RoyGoodman <bouladier.francis@gmail.com>
2026-01-24 22:36:20 -0500

    Played a bit with getting data from a webpage, got some interesting results

--- web_scan.py
+++ web_scan.py
@@ -1,7 +1,27 @@
-import urllib
+#import urllib
+import requests
+from bs4 import BeautifulSoup
 
 def main():
-    pass
+    
+    #FETCH THE PAGE
+    url = "https://www.leslibraires.ca/livres/petit-pas-9782925416760" #Test URL
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    response = requests.get(url, headers=headers)
+    html = response.text
+
+    #PARSE THE HTML
+    soup = BeautifulSoup(html, "html.parser")
+
+    items = soup.find_all("div", class_="product__description") 
+    print(len(items))
+
+    for item in items:
+        title = item.find("div", class_="d-inline text-normalizer").get_text(strip=True)
+        print("*")
+        print(title)
+
     #Figure out how to open a webpage through a .py file 
     #The program type is called a web scraper
     #The Beautiful soup library is especially well suited for this task 
