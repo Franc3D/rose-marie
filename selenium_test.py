41bc70ff03bd034282d320e1d6ac11b4cfa548e0
RoyGoodman <bouladier.francis@gmail.com>
2026-01-29 22:10:34 -0500

    Literally nothing happened, I just played in the settings of VS code to fix a few things

--- selenium_test.py
+++ selenium_test.py
@@ -32,11 +32,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
-options.add_argument("--headless=new")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
-options.add_argument("--remote-debugging-port=9222")
+options.add_argument("--headless=new") #run without a visible window
+options.add_argument("--no-sandbox") #run without the sandbox layer
+options.add_argument("--disable-dev-shm-usage") #don’t try to use GPU acceleration
+options.add_argument("--disable-gpu") #don’t use /dev/shm for shared memory
+options.add_argument("--remote-debugging-port=9222") #open a debugging port so ChromeDriver can talk to Chrome
 options.add_argument("--disable-software-rasterizer")
 
 driver = webdriver.Chrome(options=options)
